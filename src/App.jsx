acabf9d96711b562ee43578272f214c542f3f12f
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2022-12-25 21:12:39 -0300

    First steps

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import UserTable from './components/UserTable';
+import './css/index.css';
+
+function App() {
+
+
+
+  return (
+    <div className='container'>
+      <div className='dashboard'> 
+      <div className='User'>
+      <UserTable></UserTable>
+      </div>
+      <div className='info'>
+
+      </div>
+     </div>
+     <script src="/js/tamaño.js">
+      
+     </script>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UserTable.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../css/index.css'
+import Jeremy from '../images/image-jeremy.png'
+function UserTable() {
+  return (
+
+    <div className='UserComplete'>
+
+    <div className='username'>
+    <div >
+    <img className='imagen' src={Jeremy} alt="" />
+    </div>
+    </div>
+
+<div className='changeInfo'>
+
+</div>
+    </div>
+  )
+}
+
+export default UserTable
